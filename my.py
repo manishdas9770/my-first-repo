edfa75f2e4f4d892f2c8e79dc3ad79a710b599a4
manishdas9770 <manish0908@gamil.com>
2026-04-08 17:32:21 +0530

    add new code

--- my.py
+++ my.py
@@ -1,1 +1,15 @@
 print("hello world!")
+# Simple Python Program
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+for i in range(1, 6):
+    print("Number:", i)
